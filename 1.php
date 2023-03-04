f2bb01ff59b26eeac103583c9ab487a2895fa619
zedhosseini <zahrahosseinime12@gmail.com>
2023-03-04 12:25:27 +0330

    My First Project

---
+++ 1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<HTML>
+    <head>
+       <title> itsforZ </title>
+
+    </head>
+
+        <body>
+            
+            <?php
+            echo "it's just for Test";
+             ?>
+
+            <h1>WELCOME!</h1>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nulla incidunt molestiae delectus, sunt voluptatibus vitae et vel quibusdam officiis eum pariatur perspiciatis reprehenderit non voluptatum magni sequi recusandae rerum?
+
+
+        </body>
+    
+</HTML> 
